9f5ea91b23849d54238787eb5ad4a0bbe85862bf
Ayush Mishra <akumarmishra652@gmail.com>
2024-10-30 10:17:55 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,16 @@
+
+for i in range(1,6): #1 is inclusive and 20 is exclusive
+    print(i)
+
+print(" ")
+
+for i in reversed(range(1,6)):
+    print(i)
+
+print(" ")
+
+for i in range(1,21,2):
+    print(i)
+
+
+
